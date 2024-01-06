4496dfe308486c2c3cbae662c8ea5585f97e7e39
Pyeongan Ha <115601551+96smilepeace@users.noreply.github.com>
2024-01-06 09:15:42 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,2 +1,2 @@
-const solution = (my_string, n) => [...my_string].map(v =>  Array.from({length : n}, ()=>v)).flat().join(""); // 8
+const solution = (my_string, n) => [...my_string].map(v =>  Array.from({length : n}, ()=>v)).flat().join(""); // 0.08
 // const solution = (my_string, n) => [...my_string].map(v => v.repeat(n)).join("");
