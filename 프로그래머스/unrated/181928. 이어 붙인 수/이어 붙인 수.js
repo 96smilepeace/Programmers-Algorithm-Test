a73dd4a019c1136353e81b8887ea6c5e977c845f
Pyeongan Ha <115601551+96smilepeace@users.noreply.github.com>
2024-01-11 10:05:37 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -2,7 +2,7 @@ const solution = (num_list) => {
     const [even, odd] = num_list.reduce((acc, cur) => {
         const numAsString = cur.toString();
         cur % 2 === 0 ? acc[0] += numAsString : acc[1] += numAsString;
-        return acc.map(string => Number(string));
+        return acc.map(string => +string);
     },['','']);
 
     return even + odd;
